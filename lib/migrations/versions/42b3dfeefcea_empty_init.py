7c9d4d8566d70ca3894704f5dd3f9612e527ddb8
Joan Macharia <joan.macharia@students.moringa>
2023-12-07 21:02:31 +0300

    Completed lab

---
+++ lib/migrations/versions/42b3dfeefcea_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 42b3dfeefcea
+Revises: 
+Create Date: 2023-12-07 20:17:40.398971
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '42b3dfeefcea'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
